d5a1b7fafc92d3d7d51d29ab8a4c8d165a30a6fb
t16005197 <dorian.tachet@etu.univ-amu.fr>
2017-12-07 09:00:46 +0100

    CHANGEMENT D'IDE

---
+++ TD1/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<?php
+    echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+    echo 'C\'est pas mal non? <br/>';
+?>
+</body>
+</html>
