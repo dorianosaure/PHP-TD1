3639bc8604fbbdaaaa19721c6b8982a01c370cb7
Dorianosaure <dorianosaure@hotmail.fr>
2017-12-07 09:45:38 +0100

    Exo 1et 2

--- TD1/index.php
+++ TD1/index.php
@@ -8,8 +8,18 @@
 </head>
 <body>
 <?php
+	$var1 = 6;
+	$var2 = 1.3;
+	$var3 = 'Variable 3';
+
+	echo "$var1 + $var2"; 
+	echo $var1 + $var2;
+	echo $var1 + $var3;
+
     echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
     echo 'C\'est pas mal non? <br/>';
+
+    
 ?>
 </body>
 </html>
